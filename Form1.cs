db76635e70c7593ba4075ef97bb0b63cd7011b06
Amaury Huerb Simões <31355114+amaurysimoes@users.noreply.github.com>
2021-04-06 11:50:52 -0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Local de informações: http://theclub.com.br/Restrito/Revistas/201208/ling1208.aspx
+
+namespace Classes_eObjetos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Instanciando um objeto
+            Socio soc = new Socio();
+
+            //Inserindo atributos
+            soc.Nome = "Amaury Huerb";
+            soc.Rg = "xxxxxxx-x";
+            soc.Cpf = "620.780.xxx-xx";
+            soc.Nascimento = DateTime.Parse("28/01/1989");
+            soc.Telefone = "(92) 98856-30xx";
+            soc.Endereco = "Rua Dr. Mxxxxx nº:350";
+
+            //Invocando métodos
+            soc.Cadastrar();
+            if (soc.ConfirmarCadastro(1))
+                MessageBox.Show("Cadastro Confirmado!");
+            if (soc.ValidarSocio(1))
+                MessageBox.Show("Socio Validado!");
+            soc.Alterar();
+        }
+    }
+}
---
+++ Socio.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;//Adicionado para interagir com o form
+using System.Threading.Tasks;
+
+//Definida como Classe pública
+namespace Classes_eObjetos
+{
+    public class Socio
+    {
+        //Construtor da classe
+        public Socio()
+        {
+            //Vai enviar uma MessageBox lá para o form
+            MessageBox.Show("Objesto Criado com sucesso!");
+        }
+        //Destrutor da classe
+        ~Socio()
+        {
+        }
+        //Atributos/Propriedades Privadas Encapsuladas
+        private string nome;
+        private string cpf;
+        private string rg;
+        private DateTime nascimento;
+        private string telefone;
+        private string endereco;
+
+        //Atributos/Propriedades Públicas Encapsuladas
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public string Cpf
+        {
+            get { return cpf; }
+            set { cpf = value; }
+        }
+        public string Rg
+        {
+            get { return rg; }
+            set { rg = value; }
+        }
+        public DateTime Nascimento
+        //Não será public string porque DateTime já é recurso pré-definido
+        {
+            get { return nascimento; }
+            set { nascimento = value; }
+        }
+        public string Telefone
+        {
+            get { return telefone; }
+            set { telefone = value; }
+        }
+        public string Endereco
+        {
+            get { return endereco; }
+            set { endereco = value; }
+        }
+        //Métodos públicos
+        public void Cadastrar()
+        {
+            MessageBox.Show(string.Format("Cadastrando sócio...\r\nNome: {0}, - Rg: {1}, Cpf:{2}", nome, rg, cpf));
+        }
+
+        public void Confirmar()
+        {
+            var lblConfirma = "Finalizado operação";
+        }
+        
+        public bool ConfirmarCadastro(int tipo)
+        {
+            if (tipo == 1)
+                return true;
+            else
+                return true;
+        }
+        public bool ValidarSocio(int tipo)
+        {
+            if (tipo == 1)
+                return true;
+            else
+                return true;
+        }
+        public void Alterar()
+        {
+            MessageBox.Show("Alterando sócio...");
+        }
+    }
+}
